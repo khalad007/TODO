d0a36db565dc91a01fdbfadbe5320fced3c168fe
Khalad0007 <www.arifhossain49919@gmail.com>
2024-09-21 20:35:53 +0600

    Initial Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <h1>hello </h1>
+      <Button>Hello ShadCN</Button>
+    </div>
+  );
+}
+
+export default App;
